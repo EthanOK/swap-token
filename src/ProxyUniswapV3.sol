0dd8a351f575818a9e3d9a82aef8dd9514383ca0
EthanOK <sdzdj01@163.com>
2025-04-01 13:04:41 +0800

    feat: swap token for eth (uniswap v3)

--- src/ProxyUniswapV3.sol
+++ src/ProxyUniswapV3.sol
@@ -5,6 +5,7 @@ import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRoute
 import {TransferHelper} from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IPeripheryImmutableState} from "@uniswap/v3-periphery/contracts/interfaces/IPeripheryImmutableState.sol";
+import {IWETH9} from "./interfaces/IWETH9.sol";
 
 contract ProxyUniswapV3 is Ownable {
     event ProxySwapV3(
@@ -58,7 +59,7 @@ contract ProxyUniswapV3 is Ownable {
         TransferHelper.safeTransfer(tokenOut, recipient, userAmount);
         TransferHelper.safeTransfer(tokenOut, owner(), feeAmount);
 
-        emit ProxySwapV3(tokenIn, tokenOut, amountIn, userAmount, recipient);
+        emit ProxySwapV3(address(0), tokenOut, amountIn, userAmount, recipient);
 
         return userAmount;
     }
@@ -108,6 +109,57 @@ contract ProxyUniswapV3 is Ownable {
         TransferHelper.safeTransfer(tokenOut, owner(), feeAmount);
 
         emit ProxySwapV3(tokenIn, tokenOut, amountIn, userAmount, recipient);
+
+        return userAmount;
+    }
+
+    /// @notice Swap exact token for ETH, and deduct fee
+    /// @param tokenIn TokenIn
+    /// @param amountIn AmountIn
+    /// @param amountOutMin Minimum number of tokens expected to be obtained
+    /// @param fee Fee
+    /// @param recipient To address
+    function swapExactTokenForETHWithFee(
+        address tokenIn,
+        uint256 amountIn,
+        uint256 amountOutMin,
+        uint24 fee,
+        address recipient
+    ) external returns (uint256) {
+        require(amountIn > 0, "Invalid amountIn");
+        require(tokenIn != address(0), "Invalid tokenIn address");
+        require(recipient != address(0), "Invalid recipient");
+
+        address tokenOut = IPeripheryImmutableState(address(swapRouter)).WETH9();
+
+        TransferHelper.safeTransferFrom(tokenIn, msg.sender, address(this), amountIn);
+        TransferHelper.safeApprove(tokenIn, address(swapRouter), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: tokenIn,
+            tokenOut: tokenOut,
+            fee: fee,
+            recipient: address(this),
+            deadline: block.timestamp + 100,
+            amountIn: amountIn,
+            amountOutMinimum: amountOutMin,
+            sqrtPriceLimitX96: 0 // No Price Limit
+        });
+
+        uint256 amountOut = swapRouter.exactInputSingle(params);
+
+        // WETH Swap ETH
+        IWETH9(tokenOut).withdraw(amountOut);
+
+        uint256 feeAmount = (amountOut * feePercent) / 100;
+        uint256 userAmount = amountOut - feeAmount;
+
+        require(userAmount >= amountOutMin, "AmountOutMin not met");
+
+        TransferHelper.safeTransferETH(recipient, userAmount);
+        TransferHelper.safeTransferETH(owner(), feeAmount);
+
+        emit ProxySwapV3(tokenIn, address(0), amountIn, userAmount, recipient);
 
         return userAmount;
     }
--- test/ProxyUniswapV3.t.sol
+++ test/ProxyUniswapV3.t.sol
@@ -2,17 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {ProxyUniswapV3, TransferHelper} from "../src/ProxyUniswapV3.sol";
+import {ProxyUniswapV3, TransferHelper, IWETH9} from "../src/ProxyUniswapV3.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IQuoterV2} from "@uniswap/v3-periphery/contracts/interfaces/IQuoterV2.sol";
-
-interface IWETH9 is IERC20 {
-    /// @notice Deposit ether to get wrapped ether
-    function deposit() external payable;
-
-    /// @notice Withdraw wrapped ether to get ether
-    function withdraw(uint256) external;
-}
 
 contract ProxyUniswapV3Test is Test {
     ProxyUniswapV3 public proxy;
@@ -99,5 +91,29 @@ contract ProxyUniswapV3Test is Test {
         uint256 balance_user_usdc = IERC20(USDC).balanceOf(user);
 
         console.log("user Get USDC: ", balance_user_usdc);
+    }
+
+    function test_SwapExactTokenForETHWithFee() public {
+        address recipient = address(0xaaaa);
+        vm.startPrank(user);
+        uint256 payAmount = 1 ether;
+
+        console.log("user Pay 1 ETH");
+
+        // ETH -> USDC
+        uint256 userAmountOut = proxy.swapExactETHForTokenWithFee{value: payAmount}(USDC, 0, 500, user);
+        console.log("Deduct %d % Proxy Fees After:", proxy.feePercent());
+        uint256 balance_user_usdc = IERC20(USDC).balanceOf(user);
+        assertEq(balance_user_usdc, userAmountOut);
+        console.log("user Get USDC: ", balance_user_usdc);
+
+        console.log("recipient continue to swap USDC -> ETH");
+        // USDC -> ETH
+        TransferHelper.safeApprove(USDC, address(proxy), userAmountOut);
+        uint256 recipientAmountOut = proxy.swapExactTokenForETHWithFee(USDC, userAmountOut, 0, 500, recipient);
+        console.log("Deduct %d % Proxy Fees After:", proxy.feePercent());
+        assertEq(recipientAmountOut, (recipient).balance);
+        console.log("recipient Get ETH: ", recipientAmountOut);
+        vm.stopPrank();
     }
 }
